26bcef04c8202d7e5d8898464b5bd8496487d4b8
zekePhoenix <jmoses@phoenix-analytics.com>
2022-02-19 07:29:30 -0500

    initial commit

---
+++ washingtonDC.R
@@ -0,0 +1,20 @@
+# Washington DC Metro Police Department - Open Crime Data
+
+# geoJSON link for 2022 crime: https://opendata.arcgis.com/datasets/f9cc541fc8c04106a05a1a4f1e7e813c_4.geojson
+
+# libraries
+
+library(geojsonsf)
+library(sf)
+library(tidyverse)
+library(lubridate)
+
+#data
+
+dc.url <- "https://opendata.arcgis.com/datasets/f9cc541fc8c04106a05a1a4f1e7e813c_4.geojson"
+
+crime <- geojson_sf(dc.url) %>% as_tibble()
+
+crime2 <- crime %>% 
+        mutate(REPORT_DAT = ymd_hms(REPORT_DAT))
+ 
